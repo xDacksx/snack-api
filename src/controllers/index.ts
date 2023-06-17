0f3cd473ed413bd9d703afd49c4584442c1557ac
Diego Zamora <diegozar02@gmail.com>
2023-06-17 17:48:50 -0600

    create controller client

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { Gender } from "./gender.controller";
+import { Role } from "./role.controller";
 
 export const primsa = new PrismaClient();
+
+export class ControllerClient {
+    constructor() {}
+
+    get gender(): Gender {
+        return new Gender();
+    }
+
+    get role(): Role {
+        return new Role();
+    }
+}
+
+export const controller = new ControllerClient();
