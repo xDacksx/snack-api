951c857fcda07276c8cc536ef5b7681a887cb723
Diego Zamora <diegozar02@gmail.com>
2023-06-17 17:22:35 -0600

    created gender controller

---
+++ src/controllers/gender.controller.ts
@@ -0,0 +1,22 @@
+import { primsa } from ".";
+import { Gender as IGender } from "../interfaces/controllers/gender";
+
+export class Gender {
+    constructor() {}
+
+    private async Search(name: string): Promise<IGender | null> {
+        try {
+            return await primsa.gender.findUnique({ where: { name } });
+        } catch (error) {
+            return null;
+        }
+    }
+
+    public get Male() {
+        return this.Search("male");
+    }
+
+    public get Female() {
+        return this.Search("female");
+    }
+}
