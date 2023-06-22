7b3049b191fc12ba194159c573731819ffac2c2c
Diego <diegozar02@gmail.com>
2023-06-22 15:03:54 -0600

    added person controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,11 +2,16 @@ import { PrismaClient } from "@prisma/client";
 import { Gender } from "./gender.controller";
 import { Role } from "./role.controller";
 import { Database } from "./database.controller";
+import { Person } from "./person.controller";
 
 export const primsa = new PrismaClient();
 
 export class ControllerClient {
     constructor() {}
+
+    get person(): Person {
+        return new Person();
+    }
 
     get gender(): Gender {
         return new Gender();
