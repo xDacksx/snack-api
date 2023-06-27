1d3d2246e4a756be51aed27638eb27397c507dc9
Diego <diegozar02@gmail.com>
2023-06-26 18:29:17 -0600

    created findUser and signup user methods

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,54 @@
+import { controller, primsa } from ".";
 import { authSignUp } from "../interfaces/controllers/auth";
-import { Person } from "./person.controller";
+import { compare, genSalt, hash } from "bcrypt";
+import { Person, UserModel } from "../interfaces/models";
 
 export class Auth {
     constructor() {}
 
+    public async findUser(username: string): Promise<UserModel | null> {
+        try {
+            const data = await primsa.user.findUnique({ where: { username } });
+            if (data) {
+                const user: UserModel = data;
+                return user;
+            } else return null;
+        } catch (error) {
+            return null;
+        }
+    }
+
     public async signUp(data: authSignUp) {
         try {
-        } catch (error) {}
+            const { name, lastname, birthdate, genderId } = data;
+            const person = await controller.person.create({
+                name,
+                lastname,
+                birthdate,
+                genderId,
+            });
+            const clientRole = await controller.role.client;
+
+            if (person && person.id && clientRole?.id) {
+                const salt = await genSalt();
+                const passwordHashed = await hash(data.password, salt);
+
+                console.log(compare(data.password, passwordHashed));
+
+                const user: UserModel = await primsa.user.create({
+                    data: {
+                        username: data.username,
+                        password: passwordHashed,
+                        personId: person.id,
+                        roleId: clientRole.id,
+                    },
+                });
+
+                return user;
+            }
+            return null;
+        } catch (error) {
+            return null;
+        }
     }
 }
