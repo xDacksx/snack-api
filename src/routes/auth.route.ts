df94a2a6d413070355f7a302c5820a2f2f9a2b23
Diego <diegozar02@gmail.com>
2023-07-13 19:49:00 -0600

    changed endpoint name

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,15 +5,15 @@ import {
     signUp,
     sessionSignIn,
     getFirebaseApiKeys,
+    googleSignUp,
 } from "../middlewares/auth.middleware";
 import {
     signInVaidator,
     signUpVaidator,
 } from "../middlewares/validators/auth.validator";
-import { Index } from "../middlewares";
 const router: Router = Router();
 
-router.get("/firebase", getFirebaseApiKeys);
+router.get("/firebase-keys", getFirebaseApiKeys);
 router.post("/sign-up", signUpVaidator, signUp);
 router.post("/sign-in", signInVaidator, signIn);
 router.get("/verify", verifyToken, sessionSignIn);
