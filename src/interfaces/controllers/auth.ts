83292468bd8b9faf2b260e5b9fca4a3597479215
Diego <diegozar02@gmail.com>
2023-07-22 17:31:16 -0600

    created separated files interfaces for auth controller

--- src/interfaces/controllers/auth.ts
+++ src/interfaces/controllers/auth.ts
@@ -1,3 +1,6 @@
+import { ControllerResponse } from ".";
+import { UserModel } from "../models";
+
 export interface authSignUp {
     name: string;
     lastname: string;
@@ -14,3 +17,20 @@ export interface authSignIn {
     email: string;
     password: string;
 }
+
+export type controllerResSignIn = ControllerResponse<{
+    user: UserModel;
+    token: string;
+} | null>;
+
+export type controllerResSession = ControllerResponse<UserModel | null>;
+
+export type controllerResGoogle = ControllerResponse<{
+    mode: "sign-in" | "sign-up";
+    data:
+        | UserModel
+        | {
+              user: UserModel;
+              token: string;
+          };
+} | null>;
