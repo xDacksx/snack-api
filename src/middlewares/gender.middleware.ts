030bf89bf8149ef17c4a9b40938a0b80e29a7e56
Diego <diegozar02@gmail.com>
2023-07-22 20:06:13 -0600

    created get gender middleware

---
+++ src/middlewares/gender.middleware.ts
@@ -0,0 +1,25 @@
+import { controller } from "../controllers";
+import { Req, Res } from "../interfaces/middlewares.interface";
+import { ResGetGender } from "../interfaces/middlewares/gender";
+
+export const GetGender = async (
+    req: Req,
+    res: ResGetGender
+): Promise<ResGetGender> => {
+    const id = req.params.id;
+
+    const gender = await controller.gender.searchId(parseInt(id));
+
+    if (!gender)
+        return res.send({
+            data: null,
+            message: `This gender id does not belong to any gender!`,
+            errors: ["Gender not found!"],
+        });
+
+    return res.send({
+        data: gender,
+        message: `Gender ${gender?.name} found!`,
+        errors: [],
+    });
+};
