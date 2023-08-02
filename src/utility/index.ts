a1014d9b3c04d5a53dd9bdc16c76645c96a04a25
Diego <diegozar02@gmail.com>
2023-08-02 11:14:44 -0600

    created parseBoolean utility funtion

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -153,3 +153,8 @@ export function paginate(array: any[], page_size: number, page_number: number) {
     // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
     return array.slice((page_number - 1) * page_size, page_number * page_size);
 }
+
+export function parseBoolean(value: string) {
+    if (value === "true") return true;
+    else return false;
+}
