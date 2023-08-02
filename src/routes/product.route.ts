2c87ead9373f973ad6273c7e21d76ec6c42d969b
Diego <diegozar02@gmail.com>
2023-08-02 11:33:14 -0600

    updaed products route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createProductValidator } from "../middlewares/validators/product.validator";
-import { createProduct } from "../middlewares/product.middleware";
+import { createProduct, getProducts } from "../middlewares/product.middleware";
 const router: Router = Router();
 
+router.get("/", getProducts);
 router.post("/new", createProductValidator, createProduct);
 
 export { router as ProductRoute };
