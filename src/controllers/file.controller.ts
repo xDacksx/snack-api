7283e40b0d14f36938c7a600f003fa0d48dba7f8
Diego <diegozar02@gmail.com>
2023-08-02 12:12:16 -0600

    created getDirContent function on file controller

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,10 +1,11 @@
 import { UploadedFile } from "express-fileupload";
-import { FileModel } from "../interfaces/models/";
+import { Directory, FileModel } from "../interfaces/models/";
 import { resolve } from "path";
 import { ErrorMessage, getDateAsText } from "../utility";
 import { controllerResUploadFile } from "../interfaces/controllers/product";
 import { existsSync } from "fs";
 import { prisma } from ".";
+import { readdir } from "fs/promises";
 
 export class File {
     constructor() {}
@@ -40,6 +41,30 @@ export class File {
                 data: null,
                 message,
             };
+        }
+    }
+
+    public async getDirContent(path: string): Promise<Directory[]> {
+        const DirContent: Directory[] = [];
+        try {
+            if (!this.checkFile(path))
+                throw new Error(`Path "${path}" not found!`);
+
+            const folder = await readdir(path, { withFileTypes: true });
+
+            folder.forEach((item) => {
+                const Directory: Directory = {
+                    name: item.name,
+                    path: resolve(`${path}\\${item.name}`),
+                    type: item.isDirectory() ? "folder" : "file",
+                };
+                DirContent.push(Directory);
+            });
+
+            return DirContent;
+        } catch (error) {
+            if (error instanceof Error) ErrorMessage(error.message);
+            return [];
         }
     }
 
