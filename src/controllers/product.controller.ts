14921e4e2669b8b249e4cf68656b23477e3b9d82
Diego <diegozar02@gmail.com>
2023-08-09 18:40:33 -0600

    created edit product controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,4 +1,6 @@
+import { throws } from "assert";
 import { prisma } from ".";
+import { EditProductProps } from "../interfaces/controllers/product";
 import { Product as IProduct, ProductModel } from "../interfaces/models/";
 import { ErrorMessage } from "../utility";
 
@@ -26,6 +28,33 @@ export class Product {
     async create(product: IProduct): Promise<ProductModel | null> {
         try {
             return await prisma.product.create({ data: product });
+        } catch (error) {
+            if (error instanceof Error) ErrorMessage(error.message);
+            return null;
+        }
+    }
+
+    async edit(
+        product: EditProductProps,
+        id: number
+    ): Promise<ProductModel | null> {
+        try {
+            const current = await this.getProduct(id);
+            if (!current) throw new Error("This Product does not exits!");
+
+            if (!product.available) product.available = current.available;
+            if (!product.description) product.description = current.description;
+            if (!product.id) product.id = current.id;
+            if (!product.imageId) product.imageId = current.imageId;
+            if (!product.imageUrl) product.imageUrl = current.imageUrl;
+            if (!product.name) product.name = current.name;
+            if (!product.price) product.price = current.price;
+            if (!product.quantity) product.quantity = current.quantity;
+
+            return await prisma.product.update({
+                where: { id },
+                data: product,
+            });
         } catch (error) {
             if (error instanceof Error) ErrorMessage(error.message);
             return null;
--- src/interfaces/controllers/product.ts
+++ src/interfaces/controllers/product.ts
@@ -2,3 +2,14 @@ import { ControllerResponse } from ".";
 import { FileModel } from "../models";
 
 export type controllerResUploadFile = ControllerResponse<FileModel | null>;
+
+export interface EditProductProps {
+    id?: number;
+    name?: string;
+    description?: string;
+    imageId?: number;
+    imageUrl?: string;
+    quantity?: number;
+    available?: boolean;
+    price?: number;
+}
