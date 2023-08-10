b5976e6660c6236d8dea6125a9270412d32acaad
Diego <diegozar02@gmail.com>
2023-08-09 18:40:43 -0600

    created edit product route

--- src/middlewares/product.middleware.ts
+++ src/middlewares/product.middleware.ts
@@ -1,6 +1,6 @@
 import { UploadedFile } from "express-fileupload";
 import { Req, Res } from "../interfaces/middlewares.interface";
-import { controller } from "../controllers";
+import { controller, prisma } from "../controllers";
 import { Product, ProductModel } from "../interfaces/models";
 import { parseBoolean } from "../utility";
 import {
@@ -36,10 +36,63 @@ export const createProduct = async (
     });
 
     if (product) {
+        await prisma.product.update({
+            where: { id: product.id },
+            data: {
+                imageUrl: `http://localhost:5000/product/${product.id}/img`,
+            },
+        });
         return res.send({
             data: product,
             errors: [],
             message: `Product ${product.name} created!`,
+        });
+    } else {
+        return res.send({
+            data: product,
+            errors: ["Something went wrong!"],
+            message: `Something went wrong!`,
+        });
+    }
+};
+
+export const editProduct = async (
+    req: Req,
+    res: ResCreateProduct
+): Promise<ResCreateProduct> => {
+    let imageId: number | undefined = undefined;
+
+    if (req.files) {
+        const image = req.files.image as UploadedFile;
+        const { data } = await controller.file.upload(image);
+        if (data) imageId = data.id;
+    }
+
+    const id: number = parseInt(req.params.id);
+
+    const name: string = req.body.name;
+    const description: string = req.body.description;
+    const price: number = parseFloat(req.body.price);
+    const quantity: number = parseInt(req.body.quantity);
+    const available: boolean = parseBoolean(req.body.available);
+
+    const product = await controller.product.edit(
+        {
+            name,
+            description,
+            quantity,
+            available,
+            price,
+            imageId,
+        },
+        id
+    );
+
+    if (product) {
+        return res.send({
+            data: product,
+            errors: [],
+            message: `Product ${product.name} edited!`,
         });
     } else {
         return res.send({
@@ -86,7 +139,11 @@ export const getProductImg = async (
             errors: ["This id doesn't belong to any product"],
         });
     } else {
-        return res.sendFile("C:\\Users\\dacks\\Pictures\\hamburguer.png");
+        const image = await controller.file.getFile(product.imageId);
+
+        if (!image)
+            return res.sendFile("C:\\Users\\dacks\\Pictures\\hamburguer.png");
+        return res.sendFile(image.path);
     }
 };
 export const getProducts = async (
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {
     createProductValidator,
+    editProductValidator,
     getProductValidator,
 } from "../middlewares/validators/product.validator";
 import {
     createProduct,
+    editProduct,
     getProduct,
     getProductImg,
     getProducts,
@@ -14,6 +16,8 @@ const router: Router = Router();
 router.get("/", getProducts);
 router.get("/:id", getProductValidator, getProduct);
 router.get("/:id/img", getProductValidator, getProductImg);
+
+router.post("/:id/edit", editProductValidator, editProduct);
 router.post("/new", createProductValidator, createProduct);
 
 export { router as ProductRoute };
