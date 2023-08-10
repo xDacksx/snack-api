c115afaafa0029cb024f45c024c3c3a7c2da1e16
Diego <diegozar02@gmail.com>
2023-08-09 18:40:11 -0600

    created edit product validator

--- src/middlewares/validators/product.validator.ts
+++ src/middlewares/validators/product.validator.ts
@@ -7,8 +7,10 @@ import {
     FieldValidationError,
     param,
 } from "express-validator";
+import { verifyToken } from "../auth.middleware";
 
 export const createProductValidator = [
+    verifyToken,
     check("name")
         .exists()
         .withMessage("Couldn't find name field")
@@ -77,6 +79,101 @@ export const createProductValidator = [
         }
 
         if (!req.files) errors.push(FileError("Couldn't find image field"));
+        if (req.files) {
+            if (!req.files.image) {
+                errors.push(FileError("Wrong input field name"));
+            } else {
+                const file = req.files.image;
+
+                if (!("name" in file)) {
+                    errors.push(
+                        FileError(
+                            "Image value must be a single file",
+                            "array of files"
+                        )
+                    );
+                } else {
+                    if (!file.mimetype.includes("image"))
+                        errors.push(
+                            FileError("Image value must be an image", file.name)
+                        );
+                }
+            }
+        }
+
+        if (errors.length < 1) return next();
+
+        return res.send({
+            message: "You must fill the form!",
+            errors,
+        });
+    },
+];
+
+export const editProductValidator = [
+    verifyToken,
+    check("name")
+        .optional()
+
+        .notEmpty()
+        .withMessage("Name field can not be empty")
+
+        .isString()
+        .trim()
+        .withMessage("Name value must be a string value")
+
+        .isLength({ min: 2, max: 50 })
+        .withMessage("Name field must be 4-50 characters"),
+    check("description")
+        .optional()
+
+        .notEmpty()
+        .withMessage("Description field can not be empty")
+
+        .isString()
+        .trim()
+        .withMessage("Description value must be a string value")
+
+        .isLength({ min: 2, max: 175 })
+        .withMessage("Description field must be 4-175 characters"),
+
+    check("quantity")
+        .optional()
+
+        .notEmpty()
+        .withMessage("Quantity field can not be empty")
+
+        .isInt({ min: 0, max: 999 })
+        .withMessage("Quantity value must be a number value"),
+
+    check("price")
+        .optional()
+
+        .notEmpty()
+        .withMessage("Price field can not be empty")
+
+        .isFloat()
+        .withMessage("Price value must be a double value"),
+
+    check("available")
+        .optional()
+
+        .isBoolean()
+        .withMessage("Available value must be a boolean value"),
+    (req: Req, res: Res, next: Next): Res | void => {
+        const result: Result<ValidationError> = validationResult(req);
+        const errors = result.array({ onlyFirstError: true });
+
+        function FileError(msg: string, value?: any): FieldValidationError {
+            return {
+                msg,
+                type: "field",
+                location: "body",
+                path: "image",
+                value: value ?? req.files?.image,
+            };
+        }
+
         if (req.files) {
             if (!req.files.image) {
                 errors.push(FileError("Wrong input field name"));
