6e439c9808c2bca135d0e05a826a385a4bd91e6d
Diego <diegozar02@gmail.com>
2023-08-12 14:51:01 -0600

    created get deliverer list route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { changePassword, changeRole } from "../middlewares/user.middleware";
+import {
+    changePassword,
+    changeRole,
+    deliverersList,
+} from "../middlewares/user.middleware";
 import {
     changePasswordValidator,
     changeRoleValidator,
@@ -8,5 +12,6 @@ const router: Router = Router();
 
 router.patch("/change-password", changePasswordValidator, changePassword);
 router.patch("/change-role", changeRoleValidator, changeRole);
+router.get("/deliverers-list", deliverersList);
 
 export { router as UserRoute };
