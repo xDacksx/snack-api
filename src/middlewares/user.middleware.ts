57bc54b806f4f23d3edbae6d9e97613e7d3f07cd
Diego <diegozar02@gmail.com>
2023-08-12 14:50:42 -0600

    create deliveryList middleware

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -25,3 +25,43 @@ export const changeRole = async (req: Req, res: Res): Promise<Res> => {
 
     return res.json(data);
 };
+
+export const deliverersList = async (req: Req, res: Res): Promise<Res> => {
+    const users = await controller.user.getAllDeliverers();
+    const deliverers: UserInformation[] = [];
+
+    for (let i = 0; i < users.length; i++) {
+        const {
+            email,
+            name,
+            lastname,
+            createdAt,
+            updatedAt,
+            roleId,
+            genderId,
+        } = users[i];
+
+        let role: "admin" | "client" | "delivery" = "client";
+
+        const admin = await controller.role.admin;
+        const delivery = await controller.role.delivery;
+
+        if (admin && admin.id === roleId) role = "admin";
+        if (delivery && delivery.id === roleId) role = "delivery";
+
+        const gender = await controller.gender.searchId(genderId);
+
+        deliverers.push({
+            email,
+            name,
+            lastname,
+            //@ts-ignore
+            gender: gender.name,
+            role,
+            createdAt,
+            updatedAt,
+        });
+    }
+
+    return res.json(deliverers);
+};
