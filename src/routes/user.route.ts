ece7d6e37223f62e0b4617ca05bc68eb28222f2b
Diego <diegozar02@gmail.com>
2023-08-13 23:32:09 -0600

    implemented cart routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,10 +8,23 @@ import {
     changePasswordValidator,
     changeRoleValidator,
 } from "../middlewares/validators/user.validator";
+import {
+    addProduct,
+    buyCart,
+    deleteProduct,
+    getCart,
+} from "../middlewares/cart.middleware";
+import { verifyToken } from "../middlewares/auth.middleware";
+import { addProductValidator } from "../middlewares/validators/cart.validator";
 const router: Router = Router();
 
 router.patch("/change-password", changePasswordValidator, changePassword);
 router.patch("/change-role", changeRoleValidator, changeRole);
 router.get("/deliverers-list", deliverersList);
+
+router.get("/cart", verifyToken, getCart);
+router.post("/cart", addProductValidator, addProduct);
+router.delete("/cart", addProductValidator, deleteProduct);
+router.post("/cart/buy", verifyToken, buyCart);
 
 export { router as UserRoute };
