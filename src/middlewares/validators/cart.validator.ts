8f303d5e832cfc9788b7d128484862342f24b067
Diego <diegozar02@gmail.com>
2023-08-13 23:31:10 -0600

    created cart validator

---
+++ src/middlewares/validators/cart.validator.ts
@@ -0,0 +1,33 @@
+import { Next, Req, Res } from "../../interfaces/middlewares.interface";
+import {
+    validationResult,
+    Result,
+    ValidationError,
+    check,
+} from "express-validator";
+import { verifyToken } from "../auth.middleware";
+
+export const addProductValidator = [
+    verifyToken,
+    check("id")
+        .exists()
+        .withMessage("Couldn't find role name field")
+
+        .notEmpty()
+        .withMessage("Role name field can not be empty")
+
+        .isInt({ min: 1 })
+        .withMessage("Role name value must be a string value"),
+
+    (req: Req, res: Res, next: Next): Res | void => {
+        const result: Result<ValidationError> = validationResult(req);
+        const errors = result.array({ onlyFirstError: true });
+
+        if (errors.length < 1) return next();
+
+        return res.send({
+            message: "You must fill the form!",
+            errors,
+        });
+    },
+];
