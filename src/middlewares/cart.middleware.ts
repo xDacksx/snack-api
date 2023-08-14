c4c9a2cfb15dbd52265c5797cf4dd6d87fd9940f
Diego <diegozar02@gmail.com>
2023-08-13 23:31:59 -0600

    created cart middlewares

---
+++ src/middlewares/cart.middleware.ts
@@ -0,0 +1,38 @@
+import { controller } from "../controllers";
+import { UserInformation } from "../interfaces/common/auth.interface";
+import { Req, Res } from "../interfaces/middlewares.interface";
+
+export const getCart = async (req: Req, res: Res): Promise<Res> => {
+    const UserInfo: UserInformation = res.locals.data;
+
+    const { data: cart } = await controller.cart.get(UserInfo.email);
+
+    return res.json(cart);
+};
+
+export const addProduct = async (req: Req, res: Res): Promise<Res> => {
+    const { email }: UserInformation = res.locals.data;
+    const productId: number = parseInt(req.body.id);
+
+    const data = await controller.cart.addProduct({ email, productId });
+
+    return res.json(data);
+};
+
+export const deleteProduct = async (req: Req, res: Res): Promise<Res> => {
+    const { email }: UserInformation = res.locals.data;
+    const productId: number = parseInt(req.body.id);
+
+    const data = await controller.cart.deleteProduct({ email, productId });
+
+    return res.json(data);
+};
+
+export const buyCart = async (req: Req, res: Res): Promise<Res> => {
+    const { email }: UserInformation = res.locals.data;
+    const { data } = await controller.cart.buy(email);
+
+    if (data) return res.json(data);
+
+    return res.json("nada");
+};
