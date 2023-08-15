40a010066b1f93ef49c85173cd79720b41754c48
Diego <diegozar02@gmail.com>
2023-08-14 21:12:47 -0600

    created get all user orders middleware

--- src/middlewares/order.middleware.ts
+++ src/middlewares/order.middleware.ts
@@ -1,5 +1,15 @@
+import { controller } from "../controllers";
+import { UserInformation } from "../interfaces/common/auth.interface";
 import { Req, Res } from "../interfaces/middlewares.interface";
 
 export const successBought = async (req: Req, res: Res): Promise<Res> => {
     return res.json("successBought");
+};
+
+export const getAllOrders = async (req: Req, res: Res): Promise<Res> => {
+    const { email }: UserInformation = res.locals.data;
+
+    const orders = await controller.order.getAll(email);
+
+    return res.json(orders);
 };
